536fc825a4040f754a71e74f6e9e504847a28928
Adian Tsosie <addve12@yahoo.com>
2019-01-16 12:02:44 -0700

    Added part od persona

---
+++ epic/dj-get-right.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>DJ Get Righ</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+			<p><strong>Name:</strong>DJ Get Right
+				<strong>Age:</strong>23
+				<strong>Profession:</strong>
+
+			</p>
+	</body>
+</html>
