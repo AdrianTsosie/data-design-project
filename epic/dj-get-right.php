c4adcf9978c1090323ea45f74955fa0d88cf4fd5
Adian Tsosie <addve12@yahoo.com>
2019-01-16 14:04:02 -0700

    Added index.htm splash page

--- epic/dj-get-right.php
+++ epic/dj-get-right.php
@@ -13,17 +13,20 @@
 
 		<h2>Persona</h2>
 			<ul>
-				<li><strong>Name:</strong> DJ Get Right</li>
+				<li><strong>Name:</strong> DJ Get Right!</li>
 				<li><strong>Age:</strong> 23</li>
 				<li><strong>Profession:</strong> Club DJ and Promoter</li>
-				<li><strong>Technology:</strong> Mac OS User. MacBook Pro 15 OS Sierra, iPhone Xr with unlimited $G data,
-								Pioneer CDJ2000NXS players, Pioneer DJM900NXS2 mixer, uses Serto DJ Live and RekordBox.</li>
-				<li><strong>Attitudes and Behaviors:</strong>HAs been to many other DJ site for resources. Get Rigth will
-								spend a couple of hours on the right site. He is always on the major socila media platforms</li>
+				<li><strong>Location:</strong> Tuoson, AZ</li>
+				<li><strong>Technology:</strong> Mac OS User. MacBook Pro 15 OS Sierra, iPhone Xr with unlimited 4G LTE data,
+								(2)Pioneer CDJ2000NXS2 players, Pioneer DJM900NXS2 mixer, uses Serto DJ Live and RekordBox.</li>
+				<li><strong>Attitudes and Behaviors:</strong>Hss been to many other DJ sites for resources. Get Rigth will
+								spend a couple of hours on the right site. He is always on major social media platforms</li>
 				<li><strong>Frustrations and Needs:</strong> Get Right is always on the look out for the newest music to come
 								out. He has accounts on o ther sites but is looking for something new.</li>
 				<li><strong>Goals:</strong> He needs to to stay fresh with his music in order to keep the patrons happy and
 								dancing all night. He needs to find a up to date site that can give him the newest music.</li>
 			</ul>
+
+		<h3><a href="./use-case.php">DJ Get Right!'s Story</a></h3>
 	</body>
 </html>
---
+++ epic/use-case.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>User Story</title>
+	</head>
+	<body>
+		<h1>DJ Get Right!'s Story</h1>
+			<p>
+				As a user, I need to be able to find the music I'm looking for in a timely manner. Although I have a good
+				amount of time to spend on the site, i need to be able to loock through songs quickly and easily in order
+				to get as many as i can while I am on for the visit.
+			</p>
+	</body>
