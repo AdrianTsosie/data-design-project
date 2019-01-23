a525f06262e418bffda930775d92424d43b6ab72
Adian Tsosie <addve12@yahoo.com>
2019-01-23 15:08:38 -0700

    Finished up Data Project.

--- sql/oml.sql
+++ sql/oml.sql
@@ -11,4 +11,5 @@ INSERT INTO related(relatedSongId, relatedAccountId)
 SELECT songId, songArtist, songTitle, songBPM from song WHERE songId = UNHEX("a39d2adc59634feeb2600c6ac04eda75");
 
 SELECT account.accountId, account.accountActivationToken, account.accountEmail, related.relatedAccountId
-	FROM account INNERJOIN related on related.relatedAccountId = account.accountId WHERE song.songId = "Booty Call".
+	FROM account INNER JOIN related on related.relatedAccountId = account.accountId
+	WHERE account.accountId = "b672dee903ef42f1af52e86f30c8289d"
